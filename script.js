16e8761a87e411b43a7f2e112b6a55259c79cacc
Jagadeesh-Kumar-Initial-Is-S <115778774+Jagadeesh-Kumar-Initial-Is-S@users.noreply.github.com>
2022-11-14 12:57:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// Submitted or coded by Jagadeesh Kumar . S, 
+//     you can contact me through my email address which is jagadeesh_2k17@proton.me,
+//     kindly contribute some money to my Indian UPI address which is jagadeesh-kumar@ybl .
+
+    var div = document.getElementById('timer')
+
+setTimeout(()  => {
+    div.innerHTML = 10;
+    setTimeout(()  => {
+        div.innerHTML = parseInt(09);
+        setTimeout(()  => {
+            div.innerHTML = 08;
+            setTimeout(()  => {
+                div.innerHTML = 07;
+                setTimeout(()  => {
+                    div.innerHTML = 06;
+                    setTimeout(()  => {
+                        div.innerHTML = 05;
+                        setTimeout(()  => {
+                            div.innerHTML = 04;
+                            setTimeout(()  => {
+                                div.innerHTML = 03;
+                                setTimeout(()  => {
+                                    div.innerHTML = 02;
+                                    setTimeout(()  => {
+                                        div.innerHTML = 01;
+                                        setTimeout(()  => {
+                                            div.innerHTML = "Happy Independense Day";
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+
+// Submitted or coded by Jagadeesh Kumar . S, 
+//     you can contact me through my email address which is jagadeesh_2k17@proton.me,
+//     kindly contribute some money to my Indian UPI address which is jagadeesh-kumar@ybl .
